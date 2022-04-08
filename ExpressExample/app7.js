670a9f22daf5dac06e5c406a0ecce54de845d9b0
srin23 <serin4723@naver.com>
2022-04-08 14:52:18 +0900

    [5-3]미들웨어 사용하기

--- ExpressExample/app7.js
+++ ExpressExample/app7.js
@@ -6,23 +6,33 @@ var path = require('path');
 //Express의 미들웨어 불러오기
 var bodyParser = require('body-parser');
 var static = require('serve-static');
+const { emitWarning } = require('process');
 
 //Express 객체 생성
 var app = express();
 
+//기본 속성 설정
 app.set('port', process.env.PORT || 3000)
 
+//body-parser를 이용해 application/x-222-form-urlencoded 파싱
 app.use(bodyParser.urlencoded({extended : false}))
 
+//body-parser를 이용해 application/json파싱
 app.use(bodyParser.json())
-app.use('/public', static(path.join(__dirname, 'public')))
 
+//app.use('/public', static(path.join(__dirname, 'public')))  //주소창에 /public 기본 작성 -> http://localhost:3000/public/login.html
+app.use('/', static(path.join(__dirname, 'public')))    //주소창에 / 기본 작성 -> http://localhost:3000/login.html
 
 app.use(function(req, res, next){
     console.log('첫번째 미들웨어에서 요청을 처리함');  
     
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
+    // console.log(req.body.id);   -> html에서 전달받은 값
+    // console.log(req.query.id);
+    // console.log(req.body.password);  -> html에서 전달받은 값
+    // console.log(req.body.password);
+    
+    var paramId = req.body.id || req.query.id;  // ?name=test123 작성시, req.query.id가 paramId에 저장
+    var paramPassword = req.body.password || req.query.password;    //?password=test123 작성시, req.query.password가 paramId에 저장
 
     res.writeHead('200', {'Content-Type' : 'text/html; charset=utf8'})
     res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>')
